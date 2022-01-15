b45a6b1a06f981085f7125ff89862c83dd6ee431
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-01-15 20:19:32 +0100

    exoquery

---
+++ js/modif-dom.js
@@ -0,0 +1,14 @@
+// Méthode addClass()
+$('#b1').click(function() {
+    $('#monDiv').addClass('orange gras centre');
+})
+
+// Méthode removeClass()
+$('#b2').click(function() {
+    $('#monDiv').removeClass('defaut orange gras centre');
+})
+
+// Méthode toggleClass()
+$('#b3').click(function() {
+    $('#monDiv').toggleClass('defaut orange gras centre');
+})
